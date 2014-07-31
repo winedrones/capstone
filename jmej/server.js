bf0fc6d6a8e1ab1fc26900d8f5f07f8e0862b52c
Jesse Mejia <jmejia@mercycorps.org>
2014-07-30 19:16:07 -0700

    initial setup started

---
+++ jmej/server.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var config = require('./config.js');
+var consolidate = require('consolidate');
+var Handlebars = require('handlebars');
+
+var db = require('orchestrate')(config.dbKey);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('html', consolidate.handlebars);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/server-templates');
+
+var partials = "./server-templates/partials/";
+fs.readdirSync(partials).forEach(function (file) {
+  var source = fs.readFileSync(partials + file, "utf8"),
+      partial = /(.+)\.html/.exec(file).pop();
+
+  Handlebars.registerPartial(partial, source);
+});
